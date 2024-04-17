fd24ef6e10d41f94be908a984e46be19b02a8016
GCgang <powm97@gmail.com>
2024-04-17 18:27:14 +0900

    [Feat]: index.html 렌더링

--- inflearn-django/mydjango03-hottrack/hottrack/views.py
+++ inflearn-django/mydjango03-hottrack/hottrack/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+
+# 타입을 지정하면 이상한 타입추론도 줄이고 보다 빠르고 정확하게 자동완성을 제공받을 수 있다
+def index(request: HttpRequest) -> HttpResponse:  # view 함수의 반환 타입은 HttpResponse
+    return render(
+        request=request,
+        template_name="hottrack/index.html",
+    )
