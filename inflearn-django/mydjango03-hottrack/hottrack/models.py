04c36aac7bcddc8974104d658480159db2e9bb30
GCgang <powm97@gmail.com>
2024-04-18 15:34:45 +0900

    [Feat]: 장고 ORM을 이용한 Song 모델 생성

--- inflearn-django/mydjango03-hottrack/hottrack/models.py
+++ inflearn-django/mydjango03-hottrack/hottrack/models.py
@@ -2,29 +2,29 @@ from __future__ import annotations
 from dataclasses import dataclass
 from datetime import date
 from typing import Dict
-from urllib.parse import (
-    quote,
-)  # url에서 사용할 수 있는 안전한 형태의 문자열로 인코딩 하는 역할
+
+# url에서 사용할 수 있는 안전한 형태의 문자열로 인코딩 하는 역할
+from urllib.parse import quote
+
+from django.db import models
 
 
-# __init__, __repr__ 등의 메소드를 자동으로 생성해준다
-@dataclass
-class Song:
-    id: str
-    rank: int
-    album_name: str
-    name: str
-    artist_name: str
-    cover_url: str
-    lyrics: str
-    genre: str
-    release_date: date
-    like_count: int
+class Song(models.Model):
+    melon_uid = models.CharField(max_length=20, unique=True)
+    rank = models.PositiveSmallIntegerField()
+    album_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    artist_name = models.CharField(max_length=100)
+    cover_url = models.URLField()
+    lyrics = models.TextField()
+    genre = models.CharField(max_length=100)
+    release_date = models.DateField()
+    like_count = models.PositiveIntegerField()
 
     # property 데코레이터를 사용하여 메소드를 속성처럼 접근할 수 있게 한다
     @property
     def melon_detail_url(self) -> str:
-        melon_uid = quote(self.id)
+        melon_uid = quote(self.melon_uid)
         return f"https://www.melon.com/song/detail.htm?songId={melon_uid}"
 
     @property
@@ -35,7 +35,7 @@ class Song:
     @classmethod
     def from_dict(cls, data: Dict) -> Song:
         return cls(
-            id=data.get("곡일련번호"),
+            melon_uid=data.get("곡일련번호"),
             rank=int(data.get("순위")),
             album_name=data.get("앨범"),
             name=data.get("곡명"),
