97350a073abbdb1e8ca7d17eb3ed40857f36d0c8
GCgang <powm97@gmail.com>
2024-04-18 15:36:36 +0900

    [Feat]: 검색 쿼리에 따른 노래 목록 필터 기능 추가

--- inflearn-django/mydjango03-hottrack/hottrack/views.py
+++ inflearn-django/mydjango03-hottrack/hottrack/views.py
@@ -1,8 +1,6 @@
-import json
-from urllib.request import urlopen  # URL을 통해 원격 데이터에 접근할 수 있게 해준다
-
-from django.shortcuts import render
+from django.db.models import QuerySet, Q
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 
 from hottrack.models import Song
 
@@ -11,30 +9,16 @@ from hottrack.models import Song
 def index(request: HttpRequest) -> HttpResponse:  # view 함수의 반환 타입은 HttpResponse
     query = request.GET.get("query", "").strip()
 
-    melon_chart_url = "https://raw.githubusercontent.com/pyhub-kr/dump-data/main/melon/melon-20230910.json"
-    # urlopen: URL을 인자로 받고, 그 URL에 해당하는 서버에 HTTP 요청을 보내고, 서버로부터의 응답을 받는 객체를 반환한다
-    # read(): 해당 객체의 내용을 바이트 스트림으로 읽음
-    # decode("utf-8"): 바이트 형식으로 받아온 데이터를 인코딩하여 문자열로 변환
-    json_string = urlopen(melon_chart_url).read().decode("utf-8")
-
-    # 외부 필드명을 그대로 쓰기보다, 내부적으로 사용하는 필드명으로 변경하고, 필요한 메서드를 추가합니다.
-    # json.loads(json_string): json_string을 파이썬 딕셔너리로 반환
-    song_list = [Song.from_dict(song_dict) for song_dict in json.loads(json_string)]
+    song_qs: QuerySet = Song.objects.all()
 
     if query:
-        song_list = [
-            song
-            for song in song_list
-            if (
-                (query in song.name)
-                or (query in song.artist_name)
-                or (query in song.album_name)
-                # or (query in song.lyrics)
-            )
-        ]
-
+        song_qs = song_qs.filter(
+            Q(name__icontains=query)
+            | Q(artist_name__icontains=query)
+            | Q(album_name__icontains=query)
+        )
     return render(
         request=request,
         template_name="hottrack/index.html",
-        context={"song_list": song_list, "query": query},
+        context={"song_list": song_qs, "query": query},
     )
