70219958f134ea20136999f8ee309a5300132fb4
GCgang <powm97@gmail.com>
2024-04-21 14:29:35 +0900

    [Feat]: core 주소로 들어온 요청 core.urls 로 매핑

--- inflearn-django/mydjango04/mysite/urls.py
+++ inflearn-django/mydjango04/mysite/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(route="core/", view=include("core.urls")),
+    path(route="", view=lambda request: redirect("core/")),
 ]
